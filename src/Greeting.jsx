2274fa4eab909fd9a3a3d78c270a51a9af7e9021
Viktoria <doduchenko29@gmail.com>
2021-07-07 14:59:41 +0300

    add

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Greeting = (props) => {
+  const age = Math.floor(
+    (new Date() - props.birthDate) / 1000 / 60 / 60 / 24 / 365
+  );
+  
   return (
     <div className="greeting">
-      {`My name is ${props.firstName} ${props.lastName}. I'm ${props.birthDate} years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -5,18 +5,11 @@ import './index.scss';
 
 const rootElemnt = document.querySelector('#root');
 
-const age = birthDate => {
-  const nowDate = new Date()
-  return (
-    nowDate.getFullYear() - birthDate.getFullYear()
-  )
-}
-
 ReactDOM.render(
   <Greeting
     firstName="John"
     lastName="Doe"
-    birthDate={age(new Date('2004-07-01T11:32:19.566Z'))}
+    birthDate={new Date('2004-07-01T11:32:19.566Z')}
   />,
   rootElemnt
 );
