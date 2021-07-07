5e82fb8556c46e31f022f31da883e882aaec9109
Viktoria <doduchenko29@gmail.com>
2021-07-07 14:53:47 +0300

    add

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Greeting = (props) => {
   return (
     <div className="greeting">
-      My name is {props.firstName} {props.lastName}. I'm {props.birthDate} years
-      old
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${props.birthDate} years old`}
     </div>
   );
 };
+
 export default Greeting;
